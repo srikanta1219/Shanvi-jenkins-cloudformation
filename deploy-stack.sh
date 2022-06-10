304dcf02d51a6159e2f8f3c045768b018ab0dfd3
srikanta1219 <82170893+srikanta1219@users.noreply.github.com>
2022-06-10 13:53:46 +0530

    Update deploy-stack.sh

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -14,12 +14,12 @@ else
     REGION=$5
 fi
 
-if [[ "cloudformation/"$TEMPLATE_NAME != *.yaml ]]; then
+if [[ "./"$TEMPLATE_NAME != *.yaml ]]; then
     echo "CloudFormation template $TEMPLATE_NAME does not exist. Make sure the extension is *.yaml and not (*.yml)"
     exit 0
 fi
 
-if [[ "parameters/"$PARAMETERS_FILE_NAME != *.properties ]]; then
+if [[ "./"$PARAMETERS_FILE_NAME != *.properties ]]; then
     echo "CloudFormation parameters $PARAMETERS_FILE_NAME does not exist"
     exit 0
 fi
@@ -27,15 +27,15 @@ fi
 if [[ $CHANGESET_MODE == "true" ]] || [[ $CHANGESET_MODE == "True" ]]; then
     aws cloudformation deploy \
     --stack-name $STACK_NAME \
-    --template-file cloudformation/$TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
+    --template-file ./$TEMPLATE_NAME \
+    --parameter-overrides file://./$PARAMETERS_FILE_NAME \
     --capabilities CAPABILITY_NAMED_IAM \
     --region $REGION
 else
     aws cloudformation deploy \
     --stack-name $STACK_NAME \
-    --template-file cloudformation/$TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
+    --template-file ./$TEMPLATE_NAME \
+    --parameter-overrides file://./$PARAMETERS_FILE_NAME \
     --capabilities CAPABILITY_NAMED_IAM \
     --region $REGION \
     --no-execute-changeset
